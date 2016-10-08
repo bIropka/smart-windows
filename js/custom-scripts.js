9fb9fadb2dab89241a60daf961d3ff7ddae59d1a
bIropka <BrDm_100>
2016-10-09 01:56:25 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,3 +1,36 @@
-/**
- * Created by Егор on 08.10.2016.
- */
+$(document).ready(function () {
+
+    /******************************
+     ******* init scripts *********
+     ******************************/
+
+    if ($(window).width() < '992'){
+
+    } else {
+
+    }
+
+    $(window).resize(function(){
+        if ($(window).width() < '992'){
+
+        } else {
+
+        }
+    });
+
+    /******************************
+     ******* other scripts ********
+     ******************************/
+
+    $('.form-submit').click(function() {
+        $(this).parents('form').find('.form-field:invalid').addClass('invalid-field');
+        if(document.getElementById('order-checkbox').checked) {
+            document.getElementById('order-checkbox').classList.remove('invalid-field');
+        } else {
+            document.getElementById('order-checkbox').classList.add('invalid-field');
+        }
+        $(this).parents('form').find('.custom-checkbox:invalid').addClass('invalid-field');
+        $(this).parents('form').find('.no-checked').removeClass('invalid-field');
+    });
+
+});
