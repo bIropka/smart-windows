3fcad982e80c26a8fbbb3b23df41f348e8f07513
bIropka <BrDm_100>
2016-10-10 02:36:45 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -22,17 +22,6 @@ $(document).ready(function () {
      ******* other scripts ********
      ******************************/
 
-    $('.form-submit').click(function() {
-        $(this).parents('form').find('.form-field:invalid').addClass('invalid-field');
-        if(document.getElementById('order-checkbox').checked) {
-            document.getElementById('order-checkbox').classList.remove('invalid-field');
-        } else {
-            document.getElementById('order-checkbox').classList.add('invalid-field');
-        }
-        $(this).parents('form').find('.custom-checkbox:invalid').addClass('invalid-field');
-        $(this).parents('form').find('.no-checked').removeClass('invalid-field');
-    });
-
     $('.custom-select').hover(
         function(){
             if ($(window).width() > '980'){
